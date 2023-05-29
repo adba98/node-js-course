e5d179e18c874734d447da2ef672fff91d5df1f5
Oscar Bautista <oscar.adba@gmail.com>
2023-05-28 21:59:05 -0500

    :sparkles: Validate and modify user

--- 02-Express/app.js
+++ 02-Express/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const Joi = require('joi');
 
 const app = express();
 app.use(express.json());
@@ -16,21 +17,20 @@ app.get('/api/users', (_, response) => {
 });
 
 app.get('/api/users/:id', (request, response) => {
-  let user = users.find(({ id }) => id === +request.params.id);
+  let user = searchUser(+request.params.id);
   if (!user) return response.status(404).send('User not found');
   response.send(user);
 });
 
-app.get('/api/users/:year/:month', (request, response) => {
-  const { params, query } = request;
-  response.send({ params, queryString: query });
-});
-
 app.post('/api/users', (request, response) => {
   const { name } = request.body;
-  if (!name || name.length <= 2) {
-    return response.status(400).send('Enter a valid name');
+  const schema = Joi.object({ name: Joi.string().min(3).required() });
+  const { error } = schema.validate({ name: name });
+
+  if (error) {
+    return response.status(400).send(error.details[0].message);
   }
+
   const user = {
     id: users.length + 1,
     name: request.body.name,
@@ -39,6 +39,26 @@ app.post('/api/users', (request, response) => {
   response.send(user);
 });
 
+app.put('/api/users/:id', (request, response) => {
+  let user = searchUser(+request.params.id);
+  if (!user) return response.status(404).send('User not found');
+
+  const { name } = request.body;
+  const schema = Joi.object({ name: Joi.string().min(3).required() });
+  const { error } = schema.validate({ name: name });
+
+  if (error) {
+    return response.status(400).send(error.details[0].message);
+  }
+
+  user.name = name;
+  response.send(user);
+});
+
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
+
+function searchUser(userId) {
+  return users.find(({ id }) => id === userId);
+}
